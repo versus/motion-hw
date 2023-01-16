41de0df99799af6e75dd24b73c95e00de909cf76
Valentin <versus.dev@gmail.com>
2023-01-16 21:31:48 +0100

    add files to repo

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+<template>
+    <div class="flex items-center justify-center">
+      <h1 class="m-6 text-4xl font-bold cursor-pointer select-none">
+        🤹&nbsp;@vueuse/motion
+      </h1>
+    </div>
+    </template>
+
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
